39b962b8b1a08f651e3e86a0ae7167941c46b647
Aravindi Amarasinghe <AravindiA@99x.io>
2024-05-25 18:03:25 +0530

    Added descriptive comments

--- Chinook/Services/Navigation/NavigationService.cs
+++ Chinook/Services/Navigation/NavigationService.cs
@@ -4,15 +4,19 @@ namespace Chinook.Services.Navigation
 {
     public class NavigationService
     {
+        // List of navigation items
         private List<NavigationItem> navigationItems = new();
         private IPlaylistService playlistService {  get; set; }
 
+        // Event handler to invoke when the nav menu items list is changed. 
         public event EventHandler? NavMenuItemsChanged;
         public NavigationService(IPlaylistService _playlistService) 
         {
             playlistService = _playlistService;
         }
 
+        // Fetch the playlists from the database by sending the userId as a parameter, then add playlist items as the navigation items to the list. 
+        // The displayname of the item is playlist name, and Url is consisting of the playlist Id. 
         public List<NavigationItem> GetNavigationItems(string UserId)
         {
             var playLists = playlistService.GetMyPlayLists(UserId);
@@ -27,6 +31,7 @@ namespace Chinook.Services.Navigation
             return navigationItems;
         }
 
+        // We cancall this function, when adding a new item to the navigation list. And notify the Nav menu component that the navigation items are changed. 
         public void AddNavMenuItem(long playlistId, string displayName)
         {
             var navItem = new NavigationItem(displayName, $"Playlist/{playlistId}");
@@ -39,6 +44,7 @@ namespace Chinook.Services.Navigation
         }
     }
 
+    // NavigationItem has display name and the url
     public record NavigationItem(string DisplayName, string Url);
 
 }
